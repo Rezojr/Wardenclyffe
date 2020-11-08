77097aeba4b7a2c5f35caddda77efa608a99c1dc
Patryk Rędziak <patryk.redziak.v@gmail.com>
2020-11-08 19:12:07 +0100

    Initial commit - server

---
+++ server/src/main/java/com/wardenclyffe/wardenclyffe/WardenclyffeApplication.java
@@ -0,0 +1,13 @@
+package com.wardenclyffe.wardenclyffe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardenclyffeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardenclyffeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/wardenclyffe/wardenclyffe/WardenclyffeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wardenclyffe.wardenclyffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardenclyffeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
